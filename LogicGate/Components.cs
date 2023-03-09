7d36a7d360f3220d3c2c07cc54004fdf0a845181
BrnCalisario <bruno145224@gmail.com>
2023-03-09 17:01:27 -0300

    Adicionar arquivos de projeto.

---
+++ LogicGate/Components.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicGate.Components;
+
+public abstract class Component
+{
+    public bool Output { get; set; } = false;
+    protected Component[] Inputs { get; set; }
+    protected int Index { get; set; } = 0;
+
+    public Component(int inputQuantity)
+    {
+        Inputs = new Component[inputQuantity];
+    }
+
+    public virtual void SetOutput()
+    {
+        this.Output = true;
+    }
+
+    public void Connect(Component c)
+    {
+        if (Index >= Inputs.Length)
+            throw new Exception("Limite de entradas atingido");
+
+        this.Inputs[Index] = c;
+        this.Index++;
+        this.SetOutput();
+    }
+
+    public void Connect(bool Value)
+    {
+        if (Index >= Inputs.Length)
+            throw new Exception("Limite de entradas atingido");
+
+        this.Inputs[Index] = new Input(Value) { Output = Value };
+        this.Index++;
+        this.SetOutput();
+    }
+}
+
+public class Input : Component
+{
+    public Input(bool value) : base(1)
+    {
+        this.Output = value;
+    }
+}
+
+public class GateAND : Component
+{
+    public GateAND(int qtd) : base(qtd) { }
+
+    public override void SetOutput() => 
+        this.Output = !this.Inputs.Take(Index).Any(b => !b.Output);
+}
+
+public class GateOR : Component
+{ 
+    public GateOR(int qtd) : base(qtd) { }
+
+    public override void SetOutput()
+    {
+        this.Output = this.Inputs.Take(Index).Any(b => b.Output);
+    }
+}
+
+public class GateNOT : Component
+{
+
+    public GateNOT() : base(1) { }
+    public GateNOT(bool value) : base(1) => this.Output = !value;
+
+    public override void SetOutput() =>
+        this.Output = !this.Inputs[0].Output;
+}
---
+++ LogicGate/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace LogicGate;
+
+using LogicGate.Components;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        bool inputA = false;
+        bool inputB = true;
+        bool inputC = true;
+
+        var and1 = new GateAND(2);
+        var not1 = new GateNOT();
+        var or1 = new GateOR(2);
+        var not2 = new GateNOT();
+
+        and1.Connect(inputA);
+        and1.Connect(inputB);
+        not1.Connect(inputC);
+
+        or1.Connect(and1);
+        or1.Connect(not1);
+
+        not2.Connect(or1);
+
+        Console.WriteLine(not2.Output);
+
+    }
+}
+
+
